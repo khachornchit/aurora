1c4b5ce3f34e90473be1507e06f56e10c4cb2864
Kajornjit Songsaen <kajornjit@gmail.com>
2017-04-09 12:50:03 +0700

    first commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pluto_aurora.Startup))]
+namespace pluto_aurora
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
